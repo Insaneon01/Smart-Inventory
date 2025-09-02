711020749d824570b82c8576ffd9fa2d489654ff
Insaneon01 <abhisheksingh90580@gmail.com>
2025-09-02 21:53:16 +0530

    Database

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template,send_from_directory
 # from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///inventory.db'
+db = SQLAlchemy(app)
 # CORS(app)
+
+
+
+
+
+#models for database
+class Item(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(lenght=30), nullable=False, unique=True)
+    price=db.Column(db.Integer(), nullable=False)
+    barcode=db.Column(db.String(lenght=12), nullable=False, unique=True)
+    desc=db.Column(db.String(lenght=1024), nullable=False, unique=True)
+
+
+
+
+
+
+
 @app.route('/')
 def landing():
     return render_template('landing.html')
